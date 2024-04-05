9113f7f52ff0026247d4d03196456da7b79c324e
Plasticgun21 <110892742+Plasticgun21@users.noreply.github.com>
2024-04-05 10:12:32 +0200

    ascii

--- ASCIICode.js
+++ ASCIICode.js
@@ -1,6 +1,13 @@
-function displayAscii() {
+function convertToAscii() {
+    var character = document.getElementById('asciiInput').value;
+    var asciiCode = character.charCodeAt(0);
+    var outputContainer = document.getElementById('asciiOutput');
+    outputContainer.textContent = "Der ASCII-Code von '" + character + "' ist: " + asciiCode;
+}
+
+function convertToChar() {
     var asciiInput = document.getElementById('asciiInput').value;
-    var character = String.fromCharCode(asciiInput);
+    var character = String.fromCharCode(parseInt(asciiInput));
     var outputContainer = document.getElementById('asciiOutput');
     outputContainer.textContent = "Das Zeichen mit dem ASCII-Code " + asciiInput + " ist: " + character;
 }
